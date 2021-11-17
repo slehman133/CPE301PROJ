9ba2aef69e7328d1f3da226503b411b3a1d737b6
Gordon Tan <gordont@nevada.unr.edu>
2021-11-17 15:51:18 -0800

    Added Analog/Digital conversion and initial setup

--- swamp_cooler/swamp_cooler.ino
+++ swamp_cooler/swamp_cooler.ino
@@ -65,13 +65,30 @@ int getState();
 LiquidCrystal lcd(1,2,3,4,5,6); // pins in currently are just place holders
 DHT dht(DHT_IPIN, DHT11);
 
+// DIGITAL PORT B REGISTERS
+volatile unsigned char* portB = (unsigned char*) 0x25;
+volatile unsigned char* portDDRB = (unsigned char*) 0x24;
+volatile unsigned char* pinB = (unsigned char*) 0x23;
+
+// ANALOG
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADCH_DATA = (unsigned int*) 0x79;
+volatile unsigned int* my_ADCL_DATA = (unsigned int*) 0x78;
+
 void setup() {
+  adc_init();
   Serial.begin(9600);
   lcd.begin(16, 2);
+  dht.begin();
+  *portB &= 0b01111111;
+  *portDDRB &= 0b01111111;
+  *portDDRB |= 0b01111110;
 }
 
 void loop() {
-  int waterLevel = analogRead(WATER_SENSOR_INPUT);
+  int waterLevel = adc_read(WATER_SENSOR_INPUT);
   int state = getState();
   // 0 error, 1 running, 2 idle, 3 disabled
   if(state == 0){
@@ -106,7 +123,7 @@ void checkWaterLevel(int waterLevel){
 }
 
 int getState(){
-  int waterLevel = analogRead(WATER_SENSOR_INPUT);
+  int waterLevel = adc_read(WATER_SENSOR_INPUT);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   dht.read(DHT_IPIN);
@@ -134,4 +151,58 @@ int getState(){
     // disabled
     return 3;
   }
+}
+
+/* States */
+void disabled_state(){
+  
+}
+
+void idle_state(){
+  
+}
+
+void error_state(){
+  
+}
+
+void running_state(){
+  
+}
+
+/* Analog/Digital Conversion */
+void adc_init() {
+  // Register A
+  *my_ADCSRA |= 0x80; // Set Bit 7 to 1
+  *my_ADCSRA &= 0b11011111; // Clear Bit 5
+  *my_ADCSRA &= 0b11110111; // Clear Bit 3
+  *my_ADCSRA &= 0b11111000; // Clear bits 2-0
+
+  // Register B
+  *my_ADCSRB &= 0b11110111; // Clear bit 3
+  *my_ADCSRB &= 0b11111000; // Clear bit 2-0
+
+  // MUX
+  *my_ADMUX &= 0b01111111; // Clear bit 7
+  *my_ADMUX |= 0b01000000; // Set bit 6
+  *my_ADMUX &= 0b11011111; // Clear bit 5. Right adjusted result.
+  *my_ADMUX &= 0b11100000; // Clear Bits 4-0
+}
+
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // Clear Analog channel selection bits
+  *my_ADMUX &= 0b11100000;
+  *my_ADMUX &= 0b11011111;
+  // Set channel number
+  if (adc_channel_num > 7) {
+    adc_channel_num -= 8;
+    *my_ADCSRB |= 0b00001000;
+  }
+  // Set channel selection bits
+  *my_ADMUX += adc_channel_num;
+  *my_ADCSRA |= 0b01000000;
+  // Wait for conversion to complete and return result
+  while ((*my_ADCSRA & 0x40) != 0);
+  return pow(2 * (*my_ADCH_DATA & (1 << 0)), 8) + pow(2 * (*my_ADCH_DATA & (1 << 1)), 9) + *my_ADCL_DATA; 
 }
